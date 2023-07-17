f83b812c728d74d9c5da74c1fbd6a7bb15af31fe
amincpi <alaminprodhancpi@gmail.com>
2023-07-17 19:55:16 +0600

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+//https://github.com/bashleigh/nestjs-blog/blob/master/src/user/user.controller.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
